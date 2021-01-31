3f8307d1ab5dee00d112789d8248dcb364de1510
Patrick Lamba <patricklamba@gmail.com>
2021-01-31 13:38:50 +0100

    - add UserRestController

--- src/main/java/com/lamba/todolist/controllers/UserRestController.java
+++ src/main/java/com/lamba/todolist/controllers/UserRestController.java
@@ -1,4 +1,28 @@
 package com.lamba.todolist.controllers;
 
+import com.lamba.todolist.models.User;
+import com.lamba.todolist.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@CrossOrigin(value = "*", allowedHeaders = "*")
+@RequestMapping("/user")
 public class UserRestController {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserRestController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping("/info")
+    public User info(Principal principal) {
+        return userRepository.principalToUser(principal);
+    }
 }
