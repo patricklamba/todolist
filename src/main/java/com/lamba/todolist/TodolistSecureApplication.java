976fd51ce6d56c6305ecd9302a45a4ca7d1f5442
Patrick Lamba <patricklamba@gmail.com>
2021-01-31 12:56:26 +0100

    Initial commit - bootstrap project - create models, repositories and controllers package

---
+++ src/main/java/com/lamba/todolist/TodolistSecureApplication.java
@@ -0,0 +1,13 @@
+package com.lamba.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistSecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistSecureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lamba/todolist/TodolistSecureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lamba.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistSecureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
